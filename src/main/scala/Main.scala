014bf5a4490be7a966edac46299037cf7d6a7860
iRumba <reloader2003@gmail.com>
2023-09-06 15:34:06 +0700

    home work 1 (sum, scale, axpy)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,22 @@
+import module1.homework.LinearAlgebraOps
 object Main {
 
   def main(args: Array[String]): Unit = {
+    val hw1Arr1 = Array(1, 2)
+    val hw1Arr2 = Array(3, 4)
+    val hw1Sum = LinearAlgebraOps.sum(hw1Arr1, hw1Arr2)
+    println(s"Sum of vectors ${arrayToString(hw1Arr1)} and ${arrayToString(hw1Arr2)} is ${arrayToString(hw1Sum)}")
+
+    val hw1Arr3 = Array(1, 2, 3)
+    val hw1Scalar1 = 2
+    val hw1Scale = LinearAlgebraOps.scale(hw1Arr3, hw1Scalar1)
+    println(s"Scale of vector ${arrayToString(hw1Arr3)} and scalar $hw1Scalar1 is ${arrayToString(hw1Scale)}")
+
+    val hw1Axpy = LinearAlgebraOps.axpy(hw1Scalar1, hw1Arr1, hw1Arr2)
+    println(s"Axpy of vectors ${arrayToString(hw1Arr1)} and ${arrayToString(hw1Arr2)} and scalar $hw1Scalar1 is ${arrayToString(hw1Axpy)}")
     println("Hello, World!")
   }
+
+  def arrayToString(arr: Array[Int]): String =
+    s"(${arr.mkString(", ")})"
 }
--- src/main/scala/module1/homework/LinearAlgebraOps.scala
+++ src/main/scala/module1/homework/LinearAlgebraOps.scala
@@ -1,5 +1,14 @@
 package module1.homework
 
 object LinearAlgebraOps{
-  def sum(v1: Array[Int], v2: Array[Int]): Array[Int] = ???
+  def sum(v1: Array[Int], v2: Array[Int]): Array[Int] =
+    if (v1.length != v2.length) throw new Exception("Operation is not supported")
+    else v1.zip(v2).map(d => d._1 + d._2)
+
+  def scale(v: Array[Int], a: Int): Array[Int] =
+    v.map(d => d * a)
+
+  def axpy(a: Int, v1: Array[Int], v2: Array[Int]): Array[Int] =
+    if (v1.length != v2.length) throw new Exception("Operation is not supported")
+    else v1.zip(v2).map(d => a * d._1 + d._2)
 }
