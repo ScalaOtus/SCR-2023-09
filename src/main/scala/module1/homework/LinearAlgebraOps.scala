1a88346c5cc0111de1cddcbd27e074b62a9f35a3
Dmitry Balakin <mmtr.balakin@crpt.ru>
2023-09-11 08:13:40 +0300

    homework1

--- src/main/scala/module1/homework/LinearAlgebraOps.scala
+++ src/main/scala/module1/homework/LinearAlgebraOps.scala
@@ -1,17 +1,19 @@
 package module1.homework
 
 object LinearAlgebraOps{
-  def sum(v1: Array[Int], v2: Array[Int]): Array[Int] = {
-    if (v1.length != v2.length) throw new Exception("Operation is not supported")
-    v1.zip(v2).map(x => x._1 + x._2)
+
+  val sum: PartialFunction[(Array[Int], Array[Int]), Array[Int]] =  {
+    case (v1, v2) if v1.length == v2.length => v1.zip(v2).map(x => x._1 + x._2)
+    case _ => throw new Exception("Operation is not supported")
   }
 
   def scale(v1: Array[Int], a: Int): Array[Int] = {
     v1.map(x => x * a)
   }
 
-  def axpy(v1: Array[Int], v2: Array[Int], a: Int): Array[Int] = {
-    if (v1.length != v2.length) throw new Exception("Operation is not supported")
-    sum(scale(v1, a), v2)
+  val axpy: PartialFunction[(Array[Int], Array[Int], Int), Array[Int]] = {
+    case (v1, v2, a) if v1.length == v2.length => sum(scale(v1, a), v2)
+    case _ => throw new Exception("Operation is not supported")
   }
+
 }
