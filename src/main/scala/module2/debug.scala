3b58eaf8dce82dc1929992ce06f8325eb77287f7
avoronets <avoronets@naumen.ru>
2023-10-19 14:38:30 +0300

    занятие 18

--- src/main/scala/module2/debug.scala
+++ src/main/scala/module2/debug.scala
@@ -5,13 +5,12 @@ import cats.effect.IO
 object debug {
   /** Extension methods for an effect of type `IO[A]`. */
 
-  implicit class DebugHelper[A](ioa: IO[A]) {
-    /** Print to the console the value of the effect
-     * along with the thread it was computed on. */
-    def debug: IO[A] =
-      for {
+
+    implicit class DebugHelper[A](ioa: IO[A]){
+      def debug = for{
         a <- ioa
-        tn = Thread.currentThread.getName
-        _ = println(s"[${tn}] $a")
-      } yield a }
+        tn = Thread.currentThread().getName
+        _ = println(s"[$tn] $a")
+      } yield a
+    }
 }
