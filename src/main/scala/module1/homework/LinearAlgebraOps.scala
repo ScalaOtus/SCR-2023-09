df45e8463984285e9d815efca34e79c1e57e1b35
Дамир <dmingazov@htc-cs.ru>
2023-10-22 09:50:17 +0600

    homework 01 is done.

--- src/main/scala/module1/homework/LinearAlgebraOps.scala
+++ src/main/scala/module1/homework/LinearAlgebraOps.scala
@@ -1,5 +1,18 @@
 package module1.homework
 
 object LinearAlgebraOps{
-  def sum(v1: Array[Int], v2: Array[Int]): Array[Int] = ???
+  def sum(v1: Array[Int], v2: Array[Int]): Array[Int] = {
+    if (v1.length != v2.length) {
+      throw new Exception("Operation is not supported")
+    }
+    v1.zip(v2).collect({case (a, b) => a + b})
+  }
+
+  def scale(a: Int, v: Array[Int]): Array[Int] = {
+    v.map(it => it * a)
+  }
+
+  def axpy(a: Int, v1: Array[Int], v2: Array[Int]): Array[Int] = {
+    sum(scale(a, v1), v2)
+  }
 }
